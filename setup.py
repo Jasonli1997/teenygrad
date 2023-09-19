77b8163d1fc54cd0c08ddedd69548824ca38364f
Jason Li <Jason.Li@experian.com>
2023-09-19 16:23:14 -0700

    packagized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='weenygrad',
+      version='0.1',
+      description='My journey of following tinygrad by Hotz',
+      url='https://github.com/Jasonli1997/weenygrad.git',
+      author='Jason Li',
+      license='MIT',
+      packages=['weenygrad'],
+      zip_safe=False)
